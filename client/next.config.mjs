1f2e393b9e26f203d53c61c59b10a49885d74bf8
Daniel Glynn <daniel.dg.glynn@gmail.com>
2024-06-25 00:31:50 -0600

    output export

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-      }
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+  //     }
+  //   ];
+  // },
+  output: 'export',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
   }
